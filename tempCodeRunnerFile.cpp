55de2694ebc42f555b11f684fe2c57fd2226c2bf
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2024-08-05 11:59:17 +0530

    feat: Refactor C++ program to improve array input process

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,arr[50],n,x,y;
+
+    cout<<"Enter the size of array:";
+    cin>>n;
+    
+    cout<<"Enter the elements in an array:";    
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
