c196eafea2571324448478eec734cb69e3888804
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2024-08-12 09:51:03 +0530

    Code

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -10,3 +10,17 @@ int main(){
     for(i=0;i<n;i++){
         cin>>arr[i];
     }
+
+    for(i=0;i<n-1;i++){
+        for(int j =i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<"\t";
+    }
+}
